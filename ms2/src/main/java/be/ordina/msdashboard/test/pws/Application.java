ac763966d166be137d00550c28a9c0c571e20e2b
Tim Ysewyn <Tim.Ysewyn@ordina.be>
2017-03-22 20:51:17 +0100

    Initial test setup

---
+++ ms2/src/main/java/be/ordina/msdashboard/test/pws/Application.java
@@ -0,0 +1,15 @@
+package be.ordina.msdashboard.test.pws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Application {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
